53660c073beef9cdd740b2da5a9f8db13e3e20f9
Phillip <phillip.meisinger@gmx.de>
2025-03-19 19:09:39 +0100

    this turns all LEDs on

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+#define DISPLAY_SDI 8
+#define DISPLAY_RCLK 4
+#define DISPLAY_SRCLK 7
+
 typedef struct
 {
     uint16_t Ctr:8, up:1;
@@ -27,6 +31,34 @@ void setup()
 
     button_1.gpio = A1;
     pinMode(button_1.gpio, INPUT);
+
+    pinMode(DISPLAY_SDI, OUTPUT);
+    pinMode(DISPLAY_RCLK, OUTPUT);
+    pinMode(DISPLAY_SRCLK, OUTPUT);
+
+    digitalWrite(DISPLAY_SDI, LOW);
+    digitalWrite(DISPLAY_RCLK, LOW);
+    digitalWrite(DISPLAY_SRCLK, LOW);
+
+    digitalWrite(DISPLAY_RCLK,LOW);
+    for( uint8_t i = 0; i < 8; i++ )
+    {
+        digitalWrite(DISPLAY_SRCLK,LOW);
+        digitalWrite(DISPLAY_SDI,0);
+        digitalWrite(DISPLAY_SRCLK,HIGH);
+        delay(100);
+    }
+    for( uint8_t i = 8; i < 16; i++ )
+    {
+        digitalWrite(DISPLAY_SRCLK,LOW);
+        digitalWrite(DISPLAY_SDI,1);
+        digitalWrite(DISPLAY_SRCLK,HIGH);
+        delay(100);
+    }
+    
+    
+    digitalWrite(DISPLAY_RCLK,HIGH);
+    
 }
 
 void loop()
