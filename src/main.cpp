21b3fe97467a59e15bd1c1c8c77892b4b599df3a
Phillip <phillip.meisinger@gmx.de>
2025-03-19 20:44:28 +0100

    the delay breaks it and one blicks while the other glows bright

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,12 @@ void setup()
             display_send(1<<i, 1<<j);
         }
     }
-    display_send(0x01, 0x00);
+    while(1)
+    {
+        display_send(0b11111100, 0b1000);
+        display_send(0b01100000, 0b0101);
+        delay(100);
+    }
 }
 
 void loop()
