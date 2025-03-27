8d0e2a60af96287449a2b293682cd41c4de77659
Phillip <phillip.meisinger@gmx.de>
2025-03-27 15:44:42 +0100

    seems like the lego remote sends random

--- include/ir_sensor.h
+++ include/ir_sensor.h
@@ -7,9 +7,20 @@
 
 #define IR_SENSOR_VISUALISE_PIN 10 
 
+typedef enum 
+{
+    IR_DATA_ERR = -1,
+    IR_DATA_LOW, 
+    IR_DATA_HIGH, 
+    IR_DATA_START, 
+}
+ir_data_t;
+
 void init_ir_sensor();
 
 void ir_sensor_task();
+
+ir_data_t ir_range(uint32_t value);
 
 
 #endif
--- src/ir_sensor.cpp
+++ src/ir_sensor.cpp
@@ -6,6 +6,9 @@ uint32_t last_time_us = 0;
 uint8_t curr_index = 0;
 
 uint32_t ir_times[50];
+bool something_ready = 0;
+
+bool bit_array[50];
 
 static void ir_sensor_callback()
 {
@@ -15,6 +18,12 @@ static void ir_sensor_callback()
         last_time_us = now;
         now = micros(); 
         ir_times[curr_index] = now - last_time_us;
+
+        // if there is a start bit or error length => set to 0 assuming that task is called often enough
+        ir_data_t res = ir_range(ir_times[curr_index]); 
+        bit_array[curr_index] = ((res == IR_DATA_HIGH)?(bool)true:(bool)false);
+        if ( res == IR_DATA_START || res == IR_DATA_ERR ) 
+            curr_index = 0;
         curr_index++;
         digitalWrite(IR_SENSOR_VISUALISE_PIN, HIGH );
     }
@@ -35,10 +44,38 @@ void init_ir_sensor()
 
 void ir_sensor_task()
 {
-    for ( uint8_t i = 0; i < curr_index; i ++ )
+    if ( curr_index >= 16 ) 
     {
-        Serial.println(ir_times[i]);
+        // for ( uint8_t i = 0; i < curr_index; i ++ )
+        // {
+        //     Serial.print(ir_times[i]);
+        //     Serial.print(",");
+        // }
+        // Serial.println();
+        for ( uint8_t i = 0; i < 16; i++ )
+        {
+            Serial.print(bit_array[i]);
+            Serial.print(",");
+        }
+        Serial.println();
+        curr_index = 0;
+        something_ready = false;
     }
-    curr_index = 0;
+}
+
+
+ir_data_t ir_range(uint32_t value)
+{
+    if ( value < 316 )
+        return IR_DATA_ERR;
+    if ( value <= 526 )
+        return IR_DATA_LOW;
+    if ( value <= 947 )
+        return IR_DATA_HIGH;
+    if ( value <= 1597 )
+        return IR_DATA_START; 
+    
+    return IR_DATA_ERR;
+    
 }
 
