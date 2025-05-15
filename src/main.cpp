507bdd0a2b5a319f9a9b40d00a4724286dfa3cdb
Phillip <phillip.meisinger@gmx.de>
2025-05-15 07:52:36 +0200

    pwm working with one led

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,27 @@
 #endif
 
 #define LED_AMT 4
-#define PWM_T 1000000/500
-#define PWM_STEPS_AMT 30
+#define PWM_T 2000
+#define FADE_STEPS_AMT 30
 #define FADE_TIME 1000e3
 
 uint32_t start_time_us = 0;
 uint32_t led_start_time_us[LED_AMT]={};
 
-uint32_t pwm_start_time = 0;
 uint8_t pwm_value[LED_AMT] = 
 {
-   0* 0*255/3,
-   0* 1*255/3,
-   0* 2*255/3,
-   0* 3*255/3
+    0* 0*255/3,
+    0* 1*255/3,
+    0* 2*255/3,
+    0* 3*255/3,
 };
 
-bool pwm_state[LED_AMT] =
-{
-    0,0,0,0
-};
 
 uint8_t led_pin[4] = 
 {
     11,10,5,6
 };
 
-uint8_t brightness[LED_AMT] = {};
 
 void setup()
 {
@@ -46,48 +40,47 @@ void setup()
         pinMode(led_pin[i],OUTPUT);
     }
 
-    // Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    uint32_t time_us = micros();
+    unsigned long time_us = micros();
     
-    // pwm handler 
-    if ( time_us - pwm_start_time >= PWM_T )
-    {
-        pwm_start_time = time_us;
+    // Serial.println(time_us%PWM_T);
 
-        for ( uint8_t i = 0; i < LED_AMT; i++ )
-        {
-            pwm_state[i] = 1;
-        }
-    }
+    // for all pwm leds
     for ( uint8_t i = 0; i < LED_AMT; i++ )
     {
-        if( time_us - pwm_start_time >= pwm_value[i] * (PWM_T/255) )
+        if( time_us % PWM_T >= (unsigned long) pwm_value[i] * PWM_T /255 )
         {
-            pwm_state[i] = 0;
+            digitalWrite(led_pin[i],HIGH);
         }
-        
-        digitalWrite(led_pin[i],!pwm_state[i]);
-        
+        else
+        {
+            digitalWrite(led_pin[i], LOW);
+        }
     }
+
+    delayMicroseconds(rand()%20);
+    
 
     static bool up = 0;
 
     // change the values of the pwm handler
-    if ( time_us - start_time_us >= FADE_TIME )
+    if ( (time_us - start_time_us) >= (unsigned long)(FADE_TIME/FADE_STEPS_AMT) )
     {
+        // Serial.println(pwm_value[3]);
+
         start_time_us = time_us;
 
-        if ( pwm_value[0] == 255 || pwm_value[0] == 0 )
+        if ( pwm_value[3] == 255 || pwm_value[3] == 0 )
         {
             up = !up;
         }
         if ( up )
-            pwm_value[0] ++;
+            pwm_value[3] ++;
         else
-            pwm_value[0] --;
+            pwm_value[3] --;
     }
 }
