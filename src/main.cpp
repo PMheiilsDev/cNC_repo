be1900407da4d5c9fa728d48eb82bc022aa89438
Phillip <phillip.meisinger@gmx.de>
2025-05-15 07:15:57 +0200

    not working

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,34 @@
     #include "app_api.h" 
 #endif
 
-#include "display.hpp"
-#include "imperial_march.hpp"
+#define LED_AMT 4
+#define PWM_T 1000000/500
+#define PWM_STEPS_AMT 30
+#define FADE_TIME 1000e3
 
-typedef struct
+uint32_t start_time_us = 0;
+uint32_t led_start_time_us[LED_AMT]={};
+
+uint32_t pwm_start_time = 0;
+uint8_t pwm_value[LED_AMT] = 
 {
-    uint16_t Ctr:8, up:1;
-}
-fader_t;
+   0* 0*255/3,
+   0* 1*255/3,
+   0* 2*255/3,
+   0* 3*255/3
+};
 
-typedef struct 
+bool pwm_state[LED_AMT] =
 {
-    char state:1, state_pref:1;
-    uint8_t gpio;
-}
-button_t;
+    0,0,0,0
+};
 
-fader_t fader_1;
-button_t button_1;
-button_t button_buzzer;
+uint8_t led_pin[4] = 
+{
+    11,10,5,6
+};
 
-uint8_t button_task(button_t* button);
-
+uint8_t brightness[LED_AMT] = {};
 
 void setup()
 {
@@ -35,63 +41,53 @@ void setup()
     debug_init();
     #endif
 
-    init_display();
-
-    pinMode(A1,INPUT);
+    for( uint8_t i = 0; i < LED_AMT; i++ )
+    {
+        pinMode(led_pin[i],OUTPUT);
+    }
 
     // Serial.begin(9600);
 }
 
-uint8_t i = 0;
-
-bool mode = 1;
-
-bool state = 0;
-bool state_pref = 0;
-uint32_t rising_time = 0;
-uint32_t toggle_time = 0;
-
 void loop()
 {
-
-    state_pref = state;
-    state = digitalRead(A1);
-    if ( !state && state_pref )
-    {
-        rising_time = millis();
-    }
-    if ( state && !state_pref )
-    {
-        uint32_t dt = millis() - rising_time;
-        if ( dt < 500 )
-        {
-            if( mode )
-            {
-                i++;
-                i = i%6;
-            }
-        }
-        else if ( dt >= 500 )
-        {
-            mode = !mode;
-        }
-    }
-    if ( !mode )
-    {
-        if ( millis() - toggle_time >= 1000 )
-        {
-            toggle_time = millis();
-            i++;
-            i = i%6;
-        }
-    }
-
-    display_data[0] = (i==0)?(1<<7):(1<<(i+1));
+    uint32_t time_us = micros();
     
-    display_data[1] = (i==0)?(1<<7):1<<(7-i);
-    display_data[2] = seg_7_numbers[i];
+    // pwm handler 
+    if ( time_us - pwm_start_time >= PWM_T )
+    {
+        pwm_start_time = time_us;
 
-    display_data[3] = seg_7_numbers[mode];
+        for ( uint8_t i = 0; i < LED_AMT; i++ )
+        {
+            pwm_state[i] = 1;
+        }
+    }
+    for ( uint8_t i = 0; i < LED_AMT; i++ )
+    {
+        if( time_us - pwm_start_time >= pwm_value[i] * (PWM_T/255) )
+        {
+            pwm_state[i] = 0;
+        }
         
-}
+        digitalWrite(led_pin[i],!pwm_state[i]);
+        
+    }
 
+    static bool up = 0;
+
+    // change the values of the pwm handler
+    if ( time_us - start_time_us >= FADE_TIME )
+    {
+        start_time_us = time_us;
+
+        if ( pwm_value[0] == 255 || pwm_value[0] == 0 )
+        {
+            up = !up;
+        }
+        if ( up )
+            pwm_value[0] ++;
+        else
+            pwm_value[0] --;
+    }
+}
